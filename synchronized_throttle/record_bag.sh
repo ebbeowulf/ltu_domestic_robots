eb94fb97cb7785fdabdc6fe6903e78ab5d0e8402
stretch robot <stretch@no>
2024-01-26 14:52:52 -0500

    synchronized throttle and srvs

---
+++ synchronized_throttle/record_bag.sh
@@ -0,0 +1,2 @@
+rosbag record /camera_throttled/color/camera_info /camera_throttled/color/image_raw /camera_throttled/depth/camera_info /camera_throttled/depth/image_rect_raw /odom /tf /tf_static /pose2D /scan /funmap_throttled/point_cloud2
+
